d4c5e1c5473f0fdb4aa32e18d6665444546cc594
withease <abhishekbhaskar171@gmail.com>
2022-03-07 13:55:33 +0530

    Initiated OnBoarding Screen

---
+++ src/utils/slides.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: '1',
+    title: 'Join with us...',
+    description:
+      '...and get benefits. With our channel partner plan, get guaranteed customers.',
+    image: require('../assets/images/handshake.png'),
+  },
+  {
+    id: '2',
+    title: 'Join with us...',
+    description:
+      '...and get benefits. With our channel partner plan, get guaranteed customers.',
+    image: require('../assets/images/offerlist.png'),
+  },
+  {
+    id: '3',
+    title: 'Join with us...',
+    description:
+      '...and get benefits. With our channel partner plan, get guaranteed customers.',
+    image: require('../assets/images/grow.png'),
+  },
+];
