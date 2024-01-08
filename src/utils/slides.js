51d4a3debf285cdabc0abd5989bab8e7c4970ec4
chankruze <chankruze@gmail.com>
2024-01-08 15:56:45 +0530

    fixes #2 - update onboarding screen texts
    Signed-off-by: chankruze <chankruze@gmail.com>

--- src/utils/slides.js
+++ src/utils/slides.js
@@ -1,28 +1,29 @@
 export default [
   {
     id: '1',
-    title: 'Join with us',
+    title: 'Retain your Customers with Ease',
     description:
-      'and get benefits. With our channel partner plan, enterprise online and increase your customers substantially!.',
+      "Foster lasting connections with your customers! With Fydo's built-in loyalty programs, you can reward and retain your customer base. Encourage repeat visits and transform one-time customers into loyal supporters of your business.",
     image: require('../assets/images/joinus.png'),
   },
   {
     id: '2',
-    title: 'Expand your business',
+    title: 'Exclusive Deals & Offers',
     description:
-      'and share any deals & discounts of your shop with local customers of your city.',
+      'Stay ahead then your competition, Unlock the power of exclusive deals! Fydo enables you to showcase your special offers, discounts, and promotions to potential customers in real-time. Turn passerby into loyal patrons with irresistible deals.',
     image: require('../assets/images/expand.png'),
   },
   {
     id: '3',
-    title: 'Quick assessment',
-    description: `get real-time feedback from the customers about your shop's product and services!`,
+    title: 'Real-time Analytics',
+    description:
+      'Stay in control with real-time insights! Fydo provides you with analytics on customer engagement, allowing you to track the performance of your deals and adjust strategies for maximum impact.',
     image: require('../assets/images/assessment.png'),
   },
   {
     id: '4',
-    title: 'Ecnonomic growth',
+    title: 'Increased Visibility',
     description:
-      'increase footfall and enhance offline marketing through Fydo!',
+      "Boost your shop's visibility in the local market! Fydo ensures that your exclusive deals and offerings reach nearby customers, attracting foot traffic and enhancing your shop's presence.",
   },
 ];
